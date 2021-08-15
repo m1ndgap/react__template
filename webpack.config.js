400401500b4b2f13093058835acc0b89b044d008
Tim <posseslash@gmail.com>
2021-08-15 18:12:20 +0200

    added sass/postcss processing

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require(`path`);
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 module.exports = {
-    entry: `./src/index.js`,
+    entry: path.resolve(__dirname, './src/index.js'),
     output: {
-        filename: `bundle.js`,
+        filename: "bundle.js",
         path: path.join(__dirname, `public`)
     },
     devServer: {
+        host: '0.0.0.0',
+        disableHostCheck: true,
+        useLocalIp: true,
         contentBase: path.join(__dirname, `public`),
         open: false,
         port: 1337,
@@ -19,8 +24,48 @@ module.exports = {
                 use: {
                     loader: `babel-loader`,
                 },
-            }
+            },
+            {
+                test: /\.(s(a|c)ss)$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    {
+                        loader: "css-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    [
+                                        "postcss-preset-env",
+                                        {
+                                            // Options
+                                        },
+                                    ],
+                                ],
+                            },
+                        },
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
+                ]
+            },
         ],
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "css/style.css",
+            chunkFilename: "[id].css",
+        }),
+    ],
     devtool: `source-map`,
 };
